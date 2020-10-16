447cbcda3277b27cd201c08db2f888585bff6fbc
peterH105 <60723001+peterH105@users.noreply.github.com>
2020-10-16 10:34:28 +0200

    Add files via upload

---
+++ code/topo_corr.py
@@ -0,0 +1,83 @@
+import numpy as np
+from grad_inv_functions import *
+
+def topo_corr(lon,lat,bed,ice,heights,point_mass_number):
+
+    """Calculates the gravitational effect of topography, discretized into a tesseroid model, which is transformed into point masses
+    
+    Input: 
+    bed, ice - 2D arrays of ice and bedrock topography (Longitude, Latitude, Value)
+    heights_km - Height, where gravitational effect is calculated (Float)
+    point_mass_number - Number of point masses the tesseroids are converted (Float)
+    
+    Output:
+    bouguer - Vector of gravitational effect of topography"""
+    
+    #Define densities for topo corr
+    dens_rock=2670
+    dens_water=1030
+    dens_ice=917
+    reference_topo=0.0
+
+    (bed_top,bed_bottom)=construct_layers_of_model(bed[:,2],reference_topo)
+    (ice_above_top,ice_above_bottom)=construct_layers_of_model(ice[:,2],reference_topo)
+    (ice_below_top,ice_below_bottom)=construct_layers_of_model(bed[:,2]-ice_above_bottom,reference_topo) 
+    diff=ice_above_top-bed_top
+    bed[np.where(diff <0),2]=ice[np.where(diff <0),2]
+    #ice_below_top[np.where(diff <0)]=ice_below_bottom[np.where(diff <0)]
+    ice_above_bottom=np.copy(bed_top)
+    ice_above_bottom[ice_above_bottom<0]=0.0
+    count=-1
+    for i in range(0,len(ice)):
+        
+        if bed[i,2]<0 and ice[i,2]<0 and ice[i,2]!=bed[i,2] and np.abs(bed[i,2]-ice[i,2])<10:
+            bed[i,2]=ice[i,2]
+        # rock
+        if ice[i,2]>0 and bed[i,2]>0 and ice[i,2]==bed[i,2]:
+            ice_above_top[i]=0.0
+            ice_above_bottom[i]=0.0
+            ice_below_top[i]=0.0
+            ice_below_bottom[i]=0.0
+            count=count+1
+        #water       
+        if ice[i,2]<0 and bed[i,2]<0 and ice[i,2]==bed[i,2]:
+            ice_above_top[i]=0.0
+            ice_above_bottom[i]=0.0
+            ice_below_top[i]=0.0
+            ice_below_bottom[i]=0.0
+            count=count+1
+        # ice above       
+        if ice[i,2]>0 and bed[i,2]>0 and ice[i,2]>bed[i,2]:
+            ice_below_top[i]=0.0
+            ice_below_bottom[i]=0.0
+            count=count+1
+        # ice below     
+        if ice[i,2]>0 and bed[i,2]<0 and ice[i,2]>bed[i,2]:
+            bed_top[i]=0.0
+            bed_bottom[i]=0.0
+            count=count+1
+    
+    density_topo=np.ones(bed_top.shape[0])*dens_rock
+    density_topo[np.where(bed_top == 0)]=(dens_rock-dens_water)*-1
+    density_ice_above=np.ones(ice_above_top.shape[0])*dens_ice
+    density_ice_below=np.ones(ice_below_top.shape[0])*(dens_rock-dens_ice)*-1
+
+    print("Calculate topographic effect")
+    J=create_Jacobian(lon,lat,bed_top*-1,bed_bottom*-1,heights, density_topo, point_mass_number)
+    bouguer=np.sum(J.T, axis=1) 
+    print(np.amin(bouguer),np.amax(bouguer))
+    if np.any(ice_above_top) or np.any(ice_above_bottom):
+        print("Calculate Ice Effect -- Above sea level")
+        J=create_Jacobian(lon,lat,ice_above_top*-1,ice_above_bottom*-1,heights, density_ice_above, point_mass_number)
+        bouguer_ice_above=np.sum(J.T, axis=1)
+        print(np.amin(bouguer_ice_above),np.amax(bouguer_ice_above))
+        bouguer=np.copy(bouguer)+bouguer_ice_above 
+     
+    if np.any(ice_below_top) or np.any(ice_below_bottom):
+        print("Calculate Ice Effect -- Below sea level")
+        J=create_Jacobian(lon,lat,ice_below_top*-1,ice_below_bottom*-1,heights, density_ice_below, point_mass_number)
+        bouguer_ice_below=np.sum(J.T, axis=1)
+        print(np.amin(bouguer_ice_below),np.amax(bouguer_ice_below))
+        bouguer=np.copy(bouguer)-bouguer_ice_below
+
+    return bouguer
